63fcd4a0bd28430f7e5b34acf40f3d77e8f1d234
Ido Schimmel <idosch@mellanox.com>
2019-04-24 14:56:37 +0300

    devlink: Add trap groups
    XXX: Write me
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>

--- include/net/devlink.h
+++ include/net/devlink.h
@@ -35,6 +35,7 @@ struct devlink {
 	struct devlink_dpipe_headers *dpipe_headers;
 	struct list_head trap_list;
 	struct rhashtable trap_ht;
+	struct list_head trap_group_list;
 	const struct devlink_ops *ops;
 	struct device *dev;
 	possible_net_t _net;
@@ -497,6 +498,7 @@ struct devlink_trap_metadata {
  * @state_set: Callback to set trap state in underlying device
  * @init_state: Initial trap state
  * @name: Trap name
+ * @group_id: Trap group identifier
  * @id: Trap identifier
  * @metadata_in_port: Trap provides input port
  * @metadata_timestamp: Trap provides timestamp
@@ -507,9 +509,25 @@ struct devlink_trap {
 	int (*state_set)(struct devlink *devlink, u16 id, bool enable);
 	enum devlink_trap_state init_state;
 	const char *name;
+	u16 group_id;
 	u16 id;
 	u8 metadata_in_port:1,
 	   metadata_timestamp:1;
+};
+
+/**
+ * struct devlink_trap_group - Packet trap group attributes
+ * @init_state: Initial trap group state
+ * @id: Trap group identifier
+ * @name: Trap group name
+ *
+ * Describes attributes of packet trap groups that drivers register with
+ * devlink.
+ */
+struct devlink_trap_group {
+	enum devlink_trap_state init_state;
+	u16 id;
+	const char *name;
 };
 
 struct devlink_ops {
@@ -782,6 +800,12 @@ void devlink_trap_report(struct devlink *devlink,
 			 const struct devlink_trap *trap,
 			 struct sk_buff *skb,
 			 const struct devlink_trap_metadata *metadata);
+int devlink_trap_groups_register(struct devlink *devlink,
+				 const struct devlink_trap_group *groups,
+				 size_t groups_count);
+void devlink_trap_groups_unregister(struct devlink *devlink,
+				    const struct devlink_trap_group *groups,
+				    size_t groups_count);
 
 #if IS_ENABLED(CONFIG_NET_DEVLINK)
 
