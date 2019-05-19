jpirko/linux_mlxsw

devlink: Add generic packet traps
Add generic packet traps that can report dropped packets as well as
exceptions such as TTL error.

Signed-off-by: Ido Schimmel <idosch@mellanox.com>

net/core/devlink.c
@@ -7228,13 +7228,24 @@ unlock:
 }
 EXPORT_SYMBOL_GPL(devlink_region_snapshot_create);
 
-#define DEVLINK_TRAP(_id)					\
+#define DEVLINK_TRAP(_id, _type)				\
 {								\
 	.id = DEVLINK_TRAP_GENERIC_ID_##_id,			\
 	.name = DEVLINK_TRAP_GENERIC_NAME_##_id,		\
+	.type = DEVLINK_TRAP_TYPE_##_type,			\
 }
 
 static const struct devlink_trap devlink_trap_generic[] = {
+	DEVLINK_TRAP(INGRESS_SMAC_MC_DROP, DROP),
+	DEVLINK_TRAP(INGRESS_VLAN_TAG_ALLOW_DROP, DROP),
+	DEVLINK_TRAP(INGRESS_VLAN_FILTER_DROP, DROP),
+	DEVLINK_TRAP(INGRESS_STP_FILTER_DROP, DROP),
+	DEVLINK_TRAP(EMPTY_TX_LIST_DROP, DROP),
+	DEVLINK_TRAP(LOOPBACK_FILTER_DROP, DROP),
+	DEVLINK_TRAP(BLACKHOLE_ROUTE_DROP, DROP),
+	DEVLINK_TRAP(TTL_ERROR_EXCEPTION, EXCEPTION),
+	DEVLINK_TRAP(TAIL_DROP, DROP),
+	DEVLINK_TRAP(EARLY_DROP, DROP),
 };
 
 static int devlink_trap_generic_verify(const struct devlink_trap *trap)

idosch/linux

net/core/devlink.c
@@ -7198,6 +7198,17 @@ EXPORT_SYMBOL_GPL(devlink_region_snapshot_create);
 	}
 
 static const struct devlink_trap devlink_trap_generic[] = {
+	DEVLINK_TRAP(INGRESS_SMAC_MC_DROP, DROP),
+	DEVLINK_TRAP(INGRESS_VLAN_TAG_ALLOW_DROP, DROP),
+	DEVLINK_TRAP(INGRESS_VLAN_FILTER_DROP, DROP),
+	DEVLINK_TRAP(INGRESS_STP_FILTER_DROP, DROP),
+	DEVLINK_TRAP(UC_EMPTY_TX_LIST_DROP, DROP),
+	DEVLINK_TRAP(MC_EMPTY_TX_LIST_DROP, DROP),
+	DEVLINK_TRAP(UC_LOOPBACK_FILTER_DROP, DROP),
+	DEVLINK_TRAP(BLACKHOLE_ROUTE_DROP, DROP),
+	DEVLINK_TRAP(TTL_ERROR_EXCEPTION, EXCEPTION),
+	DEVLINK_TRAP(TAIL_DROP, DROP),
+	DEVLINK_TRAP(EARLY_DROP, DROP),
 };
 
 static int devlink_trap_generic_verify(const struct devlink_trap *trap)