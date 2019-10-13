6ba127f6158fbeac29c38276a73ae8ce89da6afa
Ido Schimmel <idosch@mellanox.com>
2019-10-13 21:53:01 +0300

    ipv6: Remove old route notifications and convert listeners
    Now that mlxsw as converted to use the new FIB notifications it is
    possible to delete the old ones and use the new replace / append /
    delete notifications.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -3787,15 +3787,10 @@ static int __ip6_del_rt_siblings(struct fib6_info *rt, struct fib6_config *cfg)
 								  replace_rt);
 			else
 				call_fib6_multipath_entry_notifiers(net,
-						       FIB_EVENT_ENTRY_DEL_TMP,
+						       FIB_EVENT_ENTRY_DEL,
 						       rt, rt->fib6_nsiblings,
 						       NULL);
 		}
-		call_fib6_multipath_entry_notifiers(net,
-						    FIB_EVENT_ENTRY_DEL,
-						    rt,
-						    rt->fib6_nsiblings,
-						    NULL);
 		list_for_each_entry_safe(sibling, next_sibling,
 					 &rt->fib6_siblings,
 					 fib6_siblings) {
@@ -5074,7 +5069,6 @@ static int ip6_route_multipath_add(struct fib6_config *cfg,
 {
 	struct fib6_info *rt_notif = NULL, *rt_last = NULL;
 	struct nl_info *info = &cfg->fc_nlinfo;
-	enum fib_event_type event_type;
 	struct fib6_config r_cfg;
 	struct rtnexthop *rtnh;
 	struct fib6_info *rt;
@@ -5205,7 +5199,7 @@ static int ip6_route_multipath_add(struct fib6_config *cfg,
 	 * sibling and might allow us to skip some checks in the replace case.
 	 */
 	if (ip6_route_mpath_should_notify(rt_notif)) {
-		enum fib_event_type fib_event = FIB_EVENT_ENTRY_REPLACE_TMP;
+		enum fib_event_type fib_event = FIB_EVENT_ENTRY_REPLACE;
 
 		if (rt_notif->fib6_nsiblings != nhn - 1)
 			fib_event = FIB_EVENT_ENTRY_APPEND;
@@ -5217,14 +5211,6 @@ static int ip6_route_multipath_add(struct fib6_config *cfg,
 			err_nh = NULL;
 			goto add_errout;
 		}
-	}
-	event_type = replace ? FIB_EVENT_ENTRY_REPLACE : FIB_EVENT_ENTRY_ADD;
-	err = call_fib6_multipath_entry_notifiers(info->nl_net, event_type,
-						  rt_notif, nhn - 1, extack);
-	if (err) {
-		/* Delete all the siblings that were just added */
-		err_nh = NULL;
-		goto add_errout;
 	}
 
 	/* success ... tell user about new route */
